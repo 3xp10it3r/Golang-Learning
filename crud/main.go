2913e3f8e6a0699f1e9c63bc7b5deee7dd187e32
h1575922 <digvijay.gupta@hy-vee.com>
2026-02-07 20:37:29 +0530

    basic crud

--- crud/main.go
+++ crud/main.go
@@ -84,6 +84,86 @@ func performPostRequest() {
 
 }
 
+func performUpdateRequest() {
+	todo := Todo {
+		UserId: 20,
+		Title: "Digvijay gupta",
+		Completed: false,
+	}
+
+	// convert todo struct to JSON
+	jsonData, err := json.Marshal(todo)
+	if err != nil {
+		fmt.Println("Error marshalling", err)
+		return
+	}
+
+	jsonString := string(jsonData)
+
+	jsonReader := strings.NewReader(jsonString)
+
+	myUrl := "https://jsonplaceholder.typicode.com/todos"
+
+	req, err := http.NewRequest(http.MethodPut, myUrl, jsonReader)
+	if err != nil {
+		fmt.Println("Error creating request", err)
+		return
+	}
+
+	req.Header.Set("Content-type", "application/json")
+
+	// send the req.
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+
+	defer res.Body.Close()
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error reading resp", err)
+		return
+	}
+
+	fmt.Println(string(bytes))
+	fmt.Println("resp status", res.Status)
+}
+
+func performDeleteRequest() {
+	myUrl := "https://jsonplaceholder.typicode.com/todos/1"
+
+	req, err := http.NewRequest(http.MethodDelete, myUrl, nil)
+	if err != nil {
+		fmt.Println("Error creating DELETE request", err)
+		return
+	}
+
+	// Send DELETE request.
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+
+	defer res.Body.Close()
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error reading response", err)
+		return
+	}
+
+	fmt.Println(string(bytes))
+	fmt.Println("response status", res.Status)
+}
+
 func main() {
-	performPostRequest()
+	// performGetRequest()
+	// performPostRequest()
+	// performUpdateRequest()
+	performDeleteRequest()
 }
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"helloworld/myutil"
+	"golearning/myutil"
 )
 
 func main() {
