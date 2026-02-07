3f29b057b3ce4677cbf793d92311fce1adeaf323
h1575922 <digvijay.gupta@hy-vee.com>
2026-02-07 21:01:52 +0530

    Sync Waitgroup

---
+++ sync_waitgroup/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func worker(i int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("Worker %d started.\n", i)
+	fmt.Printf("Worker %d ended.\n", i)
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+
+	for i:=1; i < 5; i++ {
+		wg.Add(1)
+		go worker(i, &wg)
+	}
+
+	wg.Wait()
+	fmt.Println("Workers task completed.")
+}
